db484028ba7716cddc66a63190554dfd82d05564
Aryan <162725412+grim-reaper-original@users.noreply.github.com>
2026-09-09 19:04:41 +0530

    Switched to Simultaneous sampling

--- ADC.c
+++ ADC.c
@@ -2,11 +2,13 @@
 
 Uint16 Raw_Current_A = 0;
 Uint16 Raw_Current_B = 0;
-Uint16 Raw_Current_C = 0;
+
 float Offset_Current_A = 2048.0f;
 float Offset_Current_B = 2048.0f;
 
-
+float Current_A=0.0f;
+float Current_B=0.0f;
+float Current_C=0.0f;
 
 void Init_ADC_CurrentSensors(void)
 {
@@ -18,14 +20,14 @@ void Init_ADC_CurrentSensors(void)
     AdcRegs.ADCTRL3.bit.ADCCLKPS = 0x03;
     AdcRegs.ADCTRL3.bit.SMODE_SEL = 1; //simultaneous sampling of ADCINA0-A7 and ADCINB0-B7
 
-    AdcRegs.ADCMAXCONV.bit.MAX_CONV1 = 0; //conversions=maxconv_value+1 = 1 which means 1 pair of conversions (because of simultaneous sampling)
+    AdcRegs.ADCMAXCONV.bit.MAX_CONV1 = 2; //conversions=maxconv_value+1 = 3 which means 3 pairs of conversions (because of simultaneous sampling for 3 pairs of pwms)
 
-    AdcRegs.ADCCHSELSEQ1.bit.CONV00 = 0x0;
-    AdcRegs.ADCCHSELSEQ1.bit.CONV01 = 0x8;
-    AdcRegs.ADCCHSELSEQ1.bit.CONV02 = 0xA;
+    AdcRegs.ADCCHSELSEQ1.bit.CONV00 = 0x0;    //ADCINA0 corresponding to phase A
+    AdcRegs.ADCCHSELSEQ1.bit.CONV01 = 0x8;    // ADCINB0 corresponding to phase B
+    AdcRegs.ADCCHSELSEQ1.bit.CONV02 = 0xA;    // ADCINB2 coresponding to phase C
 
-    AdcRegs.ADCTRL2.bit.EPWM_SOCA_SEQ1 = 1;
-    AdcRegs.ADCTRL2.bit.INT_ENA_SEQ1 = 1;
+    AdcRegs.ADCTRL2.bit.EPWM_SOCA_SEQ1 = 1; //allow SOCA trigger to start SEQ conversion
+    AdcRegs.ADCTRL2.bit.INT_ENA_SEQ1 = 1; //allow INT_SEQ1 to send interrupt requests to CPU
 
     EDIS;
 }
@@ -57,3 +59,17 @@ void Calibrate_ADC_Offsets(void)
 
     // Phase C is no longer needed since we calculate it mathematically!
 }
+
+void Read_ADC_Currents(void)
+{
+    Raw_Current_A = AdcRegs.ADCRESULT0 >> 4;
+    Raw_Current_B = AdcRegs.ADCRESULT1 >> 4;
+
+    Current_A = ((float)Raw_Current_A - Offset_Current_A) * CURRENT_GAIN;
+
+    Current_B = ((float)Raw_Current_B - Offset_Current_B) * CURRENT_GAIN;
+
+    Current_C = -(Current_A + Current_B);
+}
+
+
--- ADC.h
+++ ADC.h
@@ -1,14 +1,23 @@
 #ifndef ADC_H
 #define ADC_H
 
+#define CURRENT_GAIN 0.003222656f
+
 #include "DSP2833x_Device.h"
 #include "DSP2833x_Examples.h"
 
 extern Uint16 Raw_Current_A;
 extern Uint16 Raw_Current_B;
-extern Uint16 Raw_Current_C;
+
+extern float Offset_Current_A;
+extern float Offset_Current_B;
+
+extern float Current_A;
+extern float Current_B;
+extern float Current_C;
 
 void Init_ADC_CurrentSensors(void);
 void Calibrate_ADC_Offsets(void);
+void Read_ADC_Currents(void);
 
 #endif
