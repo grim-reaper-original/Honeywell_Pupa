bf882b7b54b2cb0b0385f70e9732b5d167f61b54
Aryan <162725412+grim-reaper-original@users.noreply.github.com>
2026-09-09 19:14:03 +0530

    Added basic Clarke transform function for Ia and Ib

--- ADC.c
+++ ADC.c
@@ -1,4 +1,8 @@
+#include "DSP2833x_Device.h"
+#include "DSP2833x_Examples.h"
 #include "ADC.h"
+
+volatile Uint32 isr_counter = 0;
 
 Uint16 Raw_Current_A = 0;
 Uint16 Raw_Current_B = 0;
@@ -10,6 +14,9 @@ float Current_A=0.0f;
 float Current_B=0.0f;
 float Current_C=0.0f;
 
+volatile float I_alpha = 0.0f;
+volatile float I_beta  = 0.0f;
+
 void Init_ADC_CurrentSensors(void)
 {
     EALLOW;
@@ -20,11 +27,10 @@ void Init_ADC_CurrentSensors(void)
     AdcRegs.ADCTRL3.bit.ADCCLKPS = 0x03;
     AdcRegs.ADCTRL3.bit.SMODE_SEL = 1; //simultaneous sampling of ADCINA0-A7 and ADCINB0-B7
 
-    AdcRegs.ADCMAXCONV.bit.MAX_CONV1 = 2; //conversions=maxconv_value+1 = 3 which means 3 pairs of conversions (because of simultaneous sampling for 3 pairs of pwms)
+    AdcRegs.ADCMAXCONV.bit.MAX_CONV1 = 0; //conversions=maxconv_value+1 = 1 which means 1 pair of conversions (because of simultaneous sampling for A0-A7, B0-B7)
 
     AdcRegs.ADCCHSELSEQ1.bit.CONV00 = 0x0;    //ADCINA0 corresponding to phase A
-    AdcRegs.ADCCHSELSEQ1.bit.CONV01 = 0x8;    // ADCINB0 corresponding to phase B
-    AdcRegs.ADCCHSELSEQ1.bit.CONV02 = 0xA;    // ADCINB2 coresponding to phase C
+
 
     AdcRegs.ADCTRL2.bit.EPWM_SOCA_SEQ1 = 1; //allow SOCA trigger to start SEQ conversion
     AdcRegs.ADCTRL2.bit.INT_ENA_SEQ1 = 1; //allow INT_SEQ1 to send interrupt requests to CPU
@@ -72,4 +78,20 @@ void Read_ADC_Currents(void)
     Current_C = -(Current_A + Current_B);
 }
 
+void Clarke_Transform(void)
+{
+    I_alpha = Current_A;
 
+    I_beta = (Current_A + 2.0f * Current_B)
+             * 0.577350269f;
+}
+
+__interrupt void adc_isr(void)
+{
+    isr_counter++; //count ADC counts
+    Read_ADC_Currents();
+
+    AdcRegs.ADCST.bit.INT_SEQ1_CLR = 1; //clear interrupt flag
+
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1; //ack PIE group
+}
--- ADC.h
+++ ADC.h
@@ -3,6 +3,8 @@
 
 #define CURRENT_GAIN 0.003222656f
 
+extern volatile Uint32 isr_counter;
+
 #include "DSP2833x_Device.h"
 #include "DSP2833x_Examples.h"
 
@@ -16,8 +18,16 @@ extern float Current_A;
 extern float Current_B;
 extern float Current_C;
 
+extern volatile float I_alpha;
+extern volatile float I_beta;
+
+
+
 void Init_ADC_CurrentSensors(void);
 void Calibrate_ADC_Offsets(void);
 void Read_ADC_Currents(void);
+void Clarke_Transform(void);
+__interrupt void adc_isr(void);
+
 
 #endif
